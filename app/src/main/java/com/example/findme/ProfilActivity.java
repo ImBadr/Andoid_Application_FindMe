7b94808737a46e041e4b1c3e57b4acf070fb0fb7
Badr <tadjer.badr@gmail.com>
2020-03-05 15:44:18 +0100

    importer des données et afficher les données

--- app/src/main/java/com/example/findme/ProfilActivity.java
+++ app/src/main/java/com/example/findme/ProfilActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,18 +18,27 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class ProfilActivity extends AppCompatActivity {
 
     Button logoutButton;
     DatabaseReference reference;
+    TextView image, username, password;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+
+        this.image = findViewById(R.id.image);
+        this.username = findViewById(R.id.username);
+        this.password = findViewById(R.id.password);
 
         /* Récuperer des données depuis FireBase
          * Ici je récupère les données en récupérant l'identifiant de l'utilisateur current
@@ -50,11 +60,19 @@ public class ProfilActivity extends AppCompatActivity {
                  * Ici --> dataSnapShot contient toutes les data de l'utilisateur courant
                  */
 
+                /*Log.i("CLE", Objects.requireNonNull(dataSnapshot.getKey()));
+                Log.i("VALUE", Objects.requireNonNull(dataSnapshot.getValue(String.class)));*/
+
                 /* Afficher le résultat dans la console
                  */
+
                 for (DataSnapshot child : dataSnapshot.getChildren()) {
-                    Log.i("KEY", Objects.requireNonNull(child.getKey()));
-                    Log.i("VALUE", Objects.requireNonNull(child.getValue(String.class)));
+                    if (Objects.equals(child.getKey(), "id"))
+                        password.setText(child.getValue(String.class));
+                    if (Objects.equals(child.getKey(), "username"))
+                        username.setText(child.getValue(String.class));
+                    if (Objects.equals(child.getKey(), "imageURL"))
+                        image.setText(child.getValue(String.class));
                 }
             }
 
